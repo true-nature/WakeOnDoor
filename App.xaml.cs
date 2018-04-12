ac98d630e1203ba93349287c225c37236a3a574a
Yasuhiro Magara <gama@users.osdn.me>
2018-04-12 22:26:12 +0900

    Wake on LAN on TWELITE on the Door

---
+++ App.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Prism.Unity.Windows;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace WakeOnDoor
+{
+    /// <summary>
+    /// 既定の Application クラスを補完するアプリケーション固有の動作を提供します。
+    /// </summary>
+    sealed partial class App : PrismUnityApplication
+    {
+        /// <summary>
+        /// 単一アプリケーション オブジェクトを初期化します。これは、実行される作成したコードの
+        ///最初の行であるため、main() または WinMain() と論理的に等価です。
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            this.NavigationService.Navigate(PageTokens.MainPage, null);
+            return Task.CompletedTask;
+        }
+
+
+        /// <summary>
+        /// 特定のページへの移動が失敗したときに呼び出されます
+        /// </summary>
+        /// <param name="sender">移動に失敗したフレーム</param>
+        /// <param name="e">ナビゲーション エラーの詳細</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// アプリケーションの実行が中断されたときに呼び出されます。
+        /// アプリケーションが終了されるか、メモリの内容がそのままで再開されるかに
+        /// かかわらず、アプリケーションの状態が保存されます。
+        /// </summary>
+        /// <param name="sender">中断要求の送信元。</param>
+        /// <param name="e">中断要求の詳細。</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: アプリケーションの状態を保存してバックグラウンドの動作があれば停止します
+            deferral.Complete();
+        }
+    }
+}
---
+++ PageTokens.cs
@@ -0,0 +1,7 @@
+﻿namespace WakeOnDoor
+{
+    public static class PageTokens
+    {
+        public const string MainPage = "Main";
+    }
+}
