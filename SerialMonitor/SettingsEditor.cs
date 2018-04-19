e34c53e7a5cd2c5a30712054b36ae5fd9d9f6af0
Yasuhiro MAGARA <y-magara@mmi-sc.co.jp>
2018-04-19 17:46:23 +0900

    Save mac address in concatenated string

--- SerialMonitor/SettingsEditor.cs
+++ SerialMonitor/SettingsEditor.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
-using System.Threading.Tasks;
 using Windows.ApplicationModel.AppService;
 using Windows.ApplicationModel.Background;
 using Windows.Foundation.Collections;
@@ -16,6 +14,7 @@ namespace SerialMonitor
     {
         private BackgroundTaskDeferral taskDeferral;
         private AppServiceConnection connection;
+        private static char[] separators = { ',', ';' };
 
         public void Run(IBackgroundTaskInstance taskInstance)
         {
@@ -39,9 +38,7 @@ namespace SerialMonitor
         private void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
         {
             var settings = ApplicationData.Current.LocalSettings;
-            object value;
-            ReadSetting(settings, nameof(Keys.MacList), typeof(HashSet<string>), out value);
-            var macList = value as HashSet<string>;
+            var macList = ReadMacList(settings);
 
             var values = new ValueSet();
             var message = args.Request.Message;
@@ -54,18 +51,19 @@ namespace SerialMonitor
                     case nameof(AppCommands.Add):
                         if (message.TryGetValue(nameof(Keys.MacAddress), out macaddr))
                         {
-                            values[nameof(Keys.Result)] = macList.Add(macaddr as string);
-                            SaveSetting(settings, nameof(Keys.MacList), macList);
+                            values[nameof(Keys.Result)] = macList.Add(macaddr as string).ToString();
+                            SaveMacList(settings, macList);
                         }
                         break;
                     case nameof(AppCommands.Remove):
                         if (message.TryGetValue(nameof(Keys.MacAddress), out macaddr))
                         {
-                            values[nameof(Keys.Result)] = macList.Remove(macaddr as string);
-                            SaveSetting(settings, nameof(Keys.MacList), macList);
+                            values[nameof(Keys.Result)] = macList.Remove(macaddr as string).ToString();
+                            SaveMacList(settings, macList);
                         }
                         break;
                     case nameof(AppCommands.Get):
+                        values[nameof(Keys.Result)] = true.ToString();
                         break;
                     default:
                         break;
@@ -80,33 +78,24 @@ namespace SerialMonitor
             var settings = ApplicationData.Current.LocalSettings;
             if (!settings.Values.ContainsKey(nameof(Keys.MacList)))
             {
-                var macList = new HashSet<string>();
-                SaveSetting(settings, nameof(Keys.MacList), macList);
+                settings.Values[nameof(Keys.MacList)] = "";
             }
         }
 
-        private static void SaveSetting(ApplicationDataContainer settings, string key, object value)
+        private static HashSet<string> ReadMacList(ApplicationDataContainer settings)
         {
-            using (var stream = new MemoryStream())
-            {
-                var serializer = new DataContractSerializer(value.GetType());
-                serializer.WriteObject(stream, value);
-                stream.Position = 0;
-                using (var reader = new StreamReader(stream))
-                {
-                    settings.Values.Add(key, reader.ReadToEnd());
-                }
-            }
+            var macArray = (settings.Values[nameof(Keys.MacList)] as string)?.Trim().Split(separators);
+            var macList = new HashSet<string>();
+            foreach (var m in macList) { macList.Add(m); }
+            return macList;
         }
 
-        private static void ReadSetting(ApplicationDataContainer settings, string key, Type t, out object value)
+        private static string SaveMacList(ApplicationDataContainer settings, HashSet<string> macList)
         {
-            var serializer = new DataContractSerializer(t);
-            var str = (string)settings.Values[key];
-            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(str)))
-            {
-                value = serializer.ReadObject(stream);
-            }
+            var joinedStr = string.Join(",", macList);
+            settings.Values[nameof(Keys.MacList)] = string.Join(",", macList);
+            return joinedStr;
         }
+
     }
 }
