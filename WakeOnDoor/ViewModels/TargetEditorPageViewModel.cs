95d3a28da91e306858ac84a7b957a7ff87766787
Yasuhiro Magara <gama@users.osdn.me>
2018-05-01 13:07:05 +0900

    Updated visual assets

---
+++ WakeOnDoor/ViewModels/TargetEditorPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WakeOnDoor.ViewModels
+{
+    public class TargetEditorPageViewModel : ViewModelBase
+    {
+        public TargetEditorPageViewModel()
+        {
+        }
+    }
+}
